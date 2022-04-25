32d1569af7f1e06efe6e03dc146fb75ab9a1b908
Francislainy Campos <francislainy.campos@hmhco.com>
2022-04-25 09:02:45 +0100

    Delete unused files

--- junit5-jupiter-starter-maven/src/test/java/com/example/project/GoogleIT.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/GoogleIT.java
@@ -3,12 +3,6 @@ package com.example.project;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-/**
- * https://vcservice.devel.hmheng-content-pipeline.br.internal/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/virtual-classroom-controller
- * <p>
- * mvn -Dtest=com.hmhco.vcservice.functional_tests.TestSuiteGoogle test
- */
-
 class GoogleIT {
 
     @Tag("google")
--- junit5-jupiter-starter-maven/src/test/java/com/example/project/TestSuiteGoogleTest.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/TestSuiteGoogleTest.java
@@ -2,13 +2,12 @@ package com.example.project;
 
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 /**
  * https://vcservice.devel.hmheng-content-pipeline.br.internal/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/virtual-classroom-controller
  * <p>
- * mvn -Dtest=com.hmhco.vcservice.functional_tests.TestSuiteGoogle test
+ * mvn -Dtest=com.example.project.TestSuiteGoogle test
  */
 
 //@SelectClasses(GoogleIntegrationTest.class)
