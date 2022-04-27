804095fa5f6a7a0949165073cf9e46864e29f301
Jared Komoroski <jkomoroski@alianza.com>
2022-04-27 15:51:35 -0600

    Troubleshoot Intellij Idea

--- junit5-jupiter-starter-maven/src/test/java/com/example/project/TestSuiteGoogleTest.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/TestSuiteGoogleTest.java
@@ -1,19 +1,13 @@
 package com.example.project;
 
-import org.junit.platform.suite.api.IncludeTags;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.IncludePackages;
 import org.junit.platform.suite.api.Suite;
 
 /**
  * mvn -Dtest=com.example.project.TestSuiteGoogle test
  */
 
-//@SelectClasses(GoogleIntegrationTest.class)
-//@SelectClasses(GoogleIT.class)
-//@SelectClasses(GoogleTest.class)
-@SelectPackages("com.example.project")
-@IncludeTags("google")
+@IncludePackages("com.example.project")
 @Suite
 class TestSuiteGoogleTest {
 }
