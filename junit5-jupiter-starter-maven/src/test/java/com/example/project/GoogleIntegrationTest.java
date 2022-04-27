fbfa83ae6d155f32eb467554897f1b3a861b3f06
Francislainy Campos <francislainy.campos@hmhco.com>
2022-04-27 09:05:44 +0100

    Delete more unused files

--- junit5-jupiter-starter-maven/src/test/java/com/example/project/GoogleIntegrationTest.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/GoogleIntegrationTest.java
@@ -3,7 +3,6 @@ package com.example.project;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-
 class GoogleIntegrationTest {
 
     @Tag("google")
--- junit5-jupiter-starter-maven/src/test/java/com/example/project/TestSuiteGoogleTest.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/TestSuiteGoogleTest.java
@@ -2,18 +2,17 @@ package com.example.project;
 
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 /**
- * https://vcservice.devel.hmheng-content-pipeline.br.internal/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/virtual-classroom-controller
- * <p>
  * mvn -Dtest=com.example.project.TestSuiteGoogle test
  */
 
 //@SelectClasses(GoogleIntegrationTest.class)
 //@SelectClasses(GoogleIT.class)
-@SelectClasses(GoogleTest.class)
-//@SelectPackages("com.example.project")
+//@SelectClasses(GoogleTest.class)
+@SelectPackages("com.example.project")
 @IncludeTags("google")
 @Suite
 class TestSuiteGoogleTest {
